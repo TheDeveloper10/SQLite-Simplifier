d443a184456352dc1c9b61ac26eb5306e0145a81
The Developer <42378817+TheDeveloper10@users.noreply.github.com>
2020-07-18 20:44:49 +0300

    Dispose function added

--- SQLiteSimplifier.cs
+++ SQLiteSimplifier.cs
@@ -77,7 +77,7 @@ namespace Simplifier.SQLite
             return strBuilder.ToString();
         }
     }
-    
+
     /// <summary>
     /// This class simplifies the basic work with SQLite.
     /// If you want to send more specified queries and/or this class doesn't have something you need you can still use ExecuteQuery/ExecuteReader/ExecuteScalar;
@@ -85,12 +85,12 @@ namespace Simplifier.SQLite
     public class SQLiteSimplifier
     {
         private SQLiteConnection conn;
-        
+
         public SQLiteConnection Connection { get { return conn; } }
 
         public SQLiteSimplifier() { }
         public SQLiteSimplifier(string connectionString) { OpenDatabase(connectionString); }
-        
+
         /// <summary>
         /// Open a database using a connection string
         /// </summary>
@@ -113,6 +113,13 @@ namespace Simplifier.SQLite
             conn.Close();
         }
         
+        public void Dispose()
+        {
+            if (conn == null)
+                throw new Exception("You cannot close a database that is not opened!");
+            conn.Dispose();
+        }
+
         /// <summary>
         /// Create a new database
         /// </summary>
@@ -120,7 +127,7 @@ namespace Simplifier.SQLite
         {
             SQLiteConnection.CreateFile(fileName);
         }
-        
+
         /// <summary>
         /// Get the names of all tables
         /// </summary>
@@ -139,7 +146,7 @@ namespace Simplifier.SQLite
                         tableNames[i] = schema.Rows[i]["TABLE_NAME"].ToString();
                 }
             }
-            
+
             return tableNames;
         }
 
@@ -155,10 +162,11 @@ namespace Simplifier.SQLite
 
                 int count = 0;
 
-                using(DataTable schema = conn.GetSchema("Tables"))
+                using (DataTable schema = conn.GetSchema("Tables"))
                     count = schema.Rows.Count;
 
-                return count; }
+                return count;
+            }
         }
 
         /// <summary>
@@ -172,10 +180,10 @@ namespace Simplifier.SQLite
             DBColumn[] columns = null;
             int columnsCount = 0;
 
-            using(DataTable schema = conn.GetSchema("COLUMNS"))
+            using (DataTable schema = conn.GetSchema("COLUMNS"))
             {
                 DataRow[] rows = schema.Select("TABLE_NAME = '" + tableName + "'");
-                if(rows.Length > 0)
+                if (rows.Length > 0)
                 {
                     columnsCount = rows.Length;
                     columns = new DBColumn[columnsCount];
@@ -189,9 +197,10 @@ namespace Simplifier.SQLite
 
                     object dataType;
                     string dataTypeStr;
-                    for(; i < columnsCount; ++i)
+                    for (; i < columnsCount; ++i)
                     {
-                        columns[i] = new DBColumn() {
+                        columns[i] = new DBColumn()
+                        {
                             name = rows[i]["COLUMN_NAME"].ToString()
                         };
 
@@ -290,7 +299,7 @@ namespace Simplifier.SQLite
             for (int i = 0; i < columns.Length; ++i)
                 strBuilder.Append(columns[i].name + ", ");
             strBuilder.Remove(strBuilder.Length - 2, 2);
-            
+
             string str = strBuilder.ToString();
             strBuilder.Clear();
             return str;
@@ -379,7 +388,7 @@ namespace Simplifier.SQLite
             using (SQLiteCommand cmd = new SQLiteCommand("TRUNCATE TABLE " + tableName))
                 cmd.ExecuteNonQuery();
         }
-        
+
         /// <summary>
         /// Get count of rows in a table
         /// </summary>
@@ -406,7 +415,7 @@ namespace Simplifier.SQLite
             int columnCount = 0;
             using (DataTable schema = conn.GetSchema("COLUMNS"))
                 columnCount = schema.Select("TABLE_NAME = '" + tableName + "'").Length;
-            
+
             return columnCount;
         }
 
@@ -420,7 +429,7 @@ namespace Simplifier.SQLite
 
             DataRow dr = null;
 
-            using(SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT * FROM " + tableName + " LIMIT 1 OFFSET " + rowIndex, conn))
+            using (SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT * FROM " + tableName + " LIMIT 1 OFFSET " + rowIndex, conn))
             {
                 using (DataTable dt = new DataTable())
                 {
@@ -471,7 +480,7 @@ namespace Simplifier.SQLite
                 cmd.ExecuteNonQuery();
             }
         }
-        
+
         /// <summary>
         /// Replace all previous values with new ones
         /// </summary>
@@ -520,8 +529,8 @@ namespace Simplifier.SQLite
             for (int i = 0; i < columns.Length; ++i)
                 columnString.Append("," + columns[i]);
             columnString.Remove(0, 1);
-            
-            using (SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT * FROM " + tableName + " ORDER BY "  + columnString.ToString() + " " + (orderBy == OrderBy.Ascending ? "ASC" : "DESC"), conn))
+
+            using (SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT * FROM " + tableName + " ORDER BY " + columnString.ToString() + " " + (orderBy == OrderBy.Ascending ? "ASC" : "DESC"), conn))
                 adapter.Fill(dt);
 
             columnString.Clear();
@@ -536,7 +545,7 @@ namespace Simplifier.SQLite
         {
             if (conn == null)
                 throw new Exception("Open a database first!");
-            
+
             DataTable dt = new DataTable();
 
             using (SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT * FROM " + tableName, conn))
@@ -568,7 +577,7 @@ namespace Simplifier.SQLite
 
             return conn.GetSchema(schema);
         }
-        
+
         /// <summary>
         /// A method equivalent to connection.ExecuteNonQuery(commandText);
         /// </summary>
